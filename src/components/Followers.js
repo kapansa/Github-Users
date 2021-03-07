17826966f19db8d2cee98db78bd709832d23c77c
kapansa <samuelkapansa@gmail.com>
2021-03-08 00:30:36 +0300

    Update User's following and followers page

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -2,14 +2,16 @@ import React, { useContext, useEffect, Fragment } from "react";
 import User from "./User";
 import GithubContext from "../context/GithubContext";
 import Loading from "./Loading";
+import { Link } from "react-router-dom";
 
 export default function Followers({ match }) {
   const githubContext = useContext(GithubContext);
   const { followers, SetFollowers } = githubContext;
+  const login = match.params.login;
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    SetFollowers(match.params.login);
+    SetFollowers(login);
     //eslint-disable-next-line
   }, []);
 
@@ -19,7 +21,9 @@ export default function Followers({ match }) {
         <Loading />
       ) : (
         <Fragment>
-          <h1 style={{ textAlign: "center" }}>Followers</h1>
+          <h1 className="follow">
+            <Link to={`/user/${login}`}>{login}'s</Link> followers
+          </h1>
           <div className="Home">
             {followers.map((user) => (
               <User
--- src/components/Following.js
+++ src/components/Following.js
@@ -2,10 +2,12 @@ import React, { useContext, useEffect, Fragment } from "react";
 import User from "./User";
 import GithubContext from "../context/GithubContext";
 import Loading from "./Loading";
+import { Link } from "react-router-dom";
 
 export default function Following({ match }) {
   const githubContext = useContext(GithubContext);
   const { following, SetFollowing } = githubContext;
+  const login = match.params.login;
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -19,7 +21,7 @@ export default function Following({ match }) {
         <Loading />
       ) : (
         <Fragment>
-        <h1 style={{textAlign: "center"}}>Following:</h1>
+        <h1 className="follow"><Link to={`/user/${login}`}>{login}'s</Link> Following:</h1>
           <div className="Home">
             {following.map((user) => (
               <User
